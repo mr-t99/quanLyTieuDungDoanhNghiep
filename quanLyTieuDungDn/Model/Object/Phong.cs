5b2e2d85737338098aa2d5a992484df671c48ca2
Tran thang <ngocthangtrantb@gmail.com>
2020-12-14 16:33:11 +0700

    loi

---
+++ quanLyTieuDungDn/Model/Object/Phong.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quanLyTieuDungDn.Model.Object
+{
+    class Phong
+    {
+        //viet j do
+
+    }
+}
