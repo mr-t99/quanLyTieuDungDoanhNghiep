0fd10b51d2481fefd0523d6909fb5b3f1658e9f5
HuyChung99 <55877806+HuyChung99@users.noreply.github.com>
2020-12-14 15:16:27 +0700

    class NguoiDung,Phong

---
+++ quanLyTieuDungDn/Model/Object/NguoiDung.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quanLyTieuDungDn.Model.Object
+{
+    class NguoiDung
+    {
+        public int id_nguoidung { get; set; }
+        public string t_khoan { get; set; }
+        public string m_khau { get; set; }
+        public string tn_dung { get; set; }
+        public string que_quan { get; set; }
+        public string nay_lam { get; set; }
+        public string c_vu { get; set; }
+        public string id_phong { get; set; }
+        public NguoiDung() { }
+
+    }
+}
